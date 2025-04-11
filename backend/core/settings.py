29d8310abf8d4f9c40dbf3009b33b44207970ad4
Carlos Santos <carlosvit26@gmail.com>
2025-04-11 20:52:48 -0300

    New ['backend'] - Modificando a linguagem do banco

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Gerencia',
+        'USER': 'postgres',
+        'PASSWORD': 'santos2018',
+        'HOST': 'localhost',
     }
 }
 
